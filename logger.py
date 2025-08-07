11b903aa4705b6b63f55925eb1287199e68275cc
VadymTeterin <161862865+VadymTeterin@users.noreply.github.com>
2025-08-07 14:20:15 +0300

    v3.0- RotatingFileHandler for logger.py

--- logger.py
+++ logger.py
@@ -1,13 +1,23 @@
 import logging
+from logging.handlers import RotatingFileHandler
 
-logging.basicConfig(
-    filename='bot.log',
-    level=logging.INFO,
-    format='%(asctime)s - %(levelname)s - %(message)s'
+# Налаштування RotatingFileHandler:
+log_handler = RotatingFileHandler(
+    "bot.log",            # ім'я основного файлу логу
+    maxBytes=5_000_000,   # максимум ~5 MB (можна змінити)
+    backupCount=3,        # зберігати останні 3 файли (bot.log.1, bot.log.2, bot.log.3)
+    encoding="utf-8"
 )
+log_handler.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+log_handler.setFormatter(formatter)
 
-def log_info(message):
-    logging.info(message)
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+logger.addHandler(log_handler)
 
-def log_error(message):
-    logging.error(message)
+def log_info(msg):
+    logger.info(msg)
+
+def log_error(msg):
+    logger.error(msg)
